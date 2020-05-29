7d5917760ba7df23ea803cf15bdc52b9e2887f9c
Ana Perez Santos <a.jperez@yahoo.com>
2020-05-29 10:00:43 -0600

    Update test to be more robust

--- spec/features/dish_show_spec.rb
+++ spec/features/dish_show_spec.rb
@@ -3,11 +3,10 @@ require "rails_helper"
 RSpec.describe "Dish Show Page", type: :feature do
   describe "when I visit a dish show page" do
     it "I see a list of its ingredients and the chef's name" do
-      # dish and chef have a one to many relationship
-      chef = Chef.create!(name: "Ramsey")
+      chef = Chef.create!(name: "Ramsay")
       dish = chef.dishes.create!(name: "Beef Wellington", description: "Ramsey's most famous dish")
+      dish_2 = chef.dishes.create!(name: "Idiot Sandwich", description: "Falls apart under pressure")
 
-      # dish and ingredient have many to many relationship
       ingredient_1 = Ingredient.create!(name: "Filet Mignon", calories: 600)
       ingredient_2 = Ingredient.create!(name: "Olive Oil", calories: 200)
       ingredient_3 = Ingredient.create!(name: "Puff Pastry", calories: 200)
@@ -19,6 +18,7 @@ RSpec.describe "Dish Show Page", type: :feature do
       visit "/dishes/#{dish.id}"
 
       expect(page).to have_content(dish.name)
+      expect(page).to_not have_content(dish_2.name)
       expect(page).to have_content(chef.name)
       expect(page).to have_content(ingredient_1.name)
       expect(page).to have_content(ingredient_2.name)
@@ -26,8 +26,3 @@ RSpec.describe "Dish Show Page", type: :feature do
     end
   end
 end
-# Story 1 of 3
-# As a visitor
-# When I visit a dish's show page
-# I see a list of ingredients for that dish
-# and the chef's name
