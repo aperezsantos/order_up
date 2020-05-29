2cf447e75ae048c46c1109255b1b7ef749404f40
Ana Perez Santos <a.jperez@yahoo.com>
2020-05-29 10:53:30 -0600

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+chef = Chef.create!(name: "Ramsay")
+dish = chef.dishes.create!(name: "Beef Wellington", description: "Ramsey's most famous dish")
+
+ingredient_1 = Ingredient.create!(name: "Filet Mignon", calories: 600)
+ingredient_2 = Ingredient.create!(name: "Olive Oil", calories: 200)
+ingredient_3 = Ingredient.create!(name: "Puff Pastry", calories: 200)
+
+DishIngredient.create!(dish: dish, ingredient: ingredient_1)
+DishIngredient.create!(dish: dish, ingredient: ingredient_2)
+DishIngredient.create!(dish: dish, ingredient: ingredient_3)
