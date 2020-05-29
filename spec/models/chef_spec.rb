878855b0b2422179889f359a7410519ed47652b3
Ana Perez Santos <a.jperez@yahoo.com>
2020-05-29 12:05:51 -0600

    Write model test for popular ingredients method

--- spec/models/chef_spec.rb
+++ spec/models/chef_spec.rb
@@ -7,4 +7,22 @@ RSpec.describe Chef, type: :model do
   describe "relationships" do
     it {should have_many :dishes}
   end
+
+  describe 'class methods' do
+    it '.popular_ingredients' do
+      chef = Chef.create!(name: "Ramsay")
+      dish_1 = chef.dishes.create!(name: "Idiot Sandwich", description: "Falls apart under pressure")
+      dish_2 = chef.dishes.create!(name: "Pasta", description: "Best undercooked")
+
+      ingredient_1 = Ingredient.create!(name: "Filet Mignon", calories: 600)
+      ingredient_2 = Ingredient.create!(name: "Olive Oil", calories: 200)
+      ingredient_3 = Ingredient.create!(name: "Puff Pastry", calories: 200)
+
+      DishIngredient.create!(dish: dish_1, ingredient: ingredient_1)
+      DishIngredient.create!(dish: dish_1, ingredient: ingredient_2)
+      DishIngredient.create!(dish: dish_2, ingredient: ingredient_1)
+
+      expect(chef.popular_ingredients).to eq(ingredient_1)
+    end
+  end
 end
